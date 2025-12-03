acc6f12ce51746c5a128405c08aef6f1fa59bd10
Johann c <johanhernan-7@hotmail.com>
2025-12-03 09:54:33 -0500

    second commit

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -16,7 +16,7 @@ export const GET: APIRoute = async({ params, request }) => {
     // Pull in your project "site" from the endpoint context
     // https://docs.astro.build/en/reference/api-reference/#site
     // site: site ?? '',
-    site: 'https://example.com',
+    site: 'https://blog.johannc.site',
     // Array of `<item>`s in output xml
     // See "Generating items" section for examples using content collections and glob imports
     items: blogPost.map(({data,slug}) =>({
